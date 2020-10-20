99a2268e4dd595d740f54b71f0422dc367d65898
greedy17 <61763082+greedy17@users.noreply.github.com>
2020-10-19 22:21:02 -0400

    destinations, restaurants, transportation and entertainment added

---
+++ scripts/dayTrip.js
@@ -0,0 +1,13 @@
+destinations = ["Bora Bora", "Japan", "Houston", "UAE", "Africa", "Hawaii", "Brazil"];
+
+boraRestaurants = ["La Matira","La villa Mahana", "Bora Bora Yacht Club"];
+japanRestaurants = ["Gyu-kaku", "CoCo's", "Yakiniku"];
+houstonRestaurants = ["Houston's", "Kata-robata", "Steak48"]
+uaeRestaurants = ["Hakkasen", "Villa Toscana", "Twin Tavern"];
+africaRestaurants = ["L'orange Bleue", "The Phatt Chef", "Reuben's"];
+hawaiiRestaurants = ["The Original Roy's", "Alan Wong's Honolulu", "Senia"];
+brazilRestaurants = ["Sushi Leblon", "Mani", "CT Boucherie"];
+
+modeOfTransportation = ["4 wheeler", "Bike", "SkateBoard", "Scooter"];
+
+formOfEntertainment = ["Horse-back Riding", "Watch a movie", "Jet ski's", "Laser tag"];
